39f5ec660a3978cc5d2586ab910598d69ae03a85
Denner Azevedo <denner.gomes@ciahering.com.br>
2023-10-15 14:32:43 -0300

    WIP

--- src/main/java/dev/dnnr/todolist/task/TaskController.java
+++ src/main/java/dev/dnnr/todolist/task/TaskController.java
@@ -1,8 +1,11 @@
 package dev.dnnr.todolist.task;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,10 +21,22 @@ public class TaskController {
   private ITaskRepository taskRepository;
   
   @PostMapping("/")
-  public TaskModel create(@RequestBody TaskModel task, HttpServletRequest request){
+  public ResponseEntity create(@RequestBody TaskModel task, HttpServletRequest request){
     var idUser = request.getAttribute("idUser");
     task.setIdUser((UUID) idUser);
+
+    var currentDate = LocalDateTime.now();
+    if(currentDate.isBefore(task.getStartAt()) || currentDate.isAfter(task.getEndAt())){
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body("A data do início/término deve ser maior que a data atual");
+    }
+
+    if(task.getStartAt().isAfter(task.getEndAt())){
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body("A data de início deve ser menor que a data de término");
+    }
+
     var response = this.taskRepository.save(task);
-    return response;
+    return ResponseEntity.status(HttpStatus.OK).body(response);
   }
 }
