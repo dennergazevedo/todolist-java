606035b0c02a778563bf8993f7f1bda7b6da74c7
Denner Azevedo <denner.gomes@ciahering.com.br>
2023-10-15 12:27:16 -0300

    Criação da tabela users + regras da DB

--- src/main/java/dev/dnnr/todolist/user/UserController.java
+++ src/main/java/dev/dnnr/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package dev.dnnr.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+  @Autowired
+  private IUserRepository userRepository;
   
   @PostMapping("/")
-  public void create(@RequestBody UserModel user){
-    System.out.println(user.getUsername());
+  public ResponseEntity create(@RequestBody UserModel user){
+    var existUser = this.userRepository.findByUsername(user.getUsername());
+
+    if(existUser != null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+    }
+
+    var userCreated = this.userRepository.save(user);
+    return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
   }
 }
